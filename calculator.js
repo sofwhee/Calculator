9ea9f8ebdc5ba3308f8d6aca12011cb6de1a7d87
sofwhee <sofiamsola@outlook.com>
2023-01-27 13:48:21 -0500

    add functions

---
+++ calculator.js
@@ -0,0 +1,24 @@
+
+function add() {
+
+}
+
+function subtract() {
+    
+}
+
+function divide() {
+    
+}
+
+function multiply() {
+    
+}
+
+function operate() {
+
+}
+
+function display() {
+    
+}
