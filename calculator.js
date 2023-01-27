c1e21aa91b0e12b65b31f4d209d6099d16e8ed3c
sofwhee <sofiamsola@outlook.com>
2023-01-27 13:53:58 -0500

    fill in math functions

--- calculator.js
+++ calculator.js
@@ -1,24 +1,33 @@
 
 function add(num1, num2) {
-
-}
+	return num1 + num2;
+};
 
 function subtract(num1, num2) {
-    
-}
+	return num1 - num2;
+};
 
 function divide(num1, num2) {
-    
-}
+	return num1 / num2;
+};
 
 function multiply(num1, num2) {
-    
-}
+  return num1 * num2;
+};
 
 function operate(num1, num2, operand) {
-
-}
+	switch(operand) {
+		case "+":
+			add(num1, num2);
+		case "-":
+			subtract(num1, num2);
+		case "/":
+			divide(num1, num2);
+		case "*":
+			multiply(num1, num2);
+	}
+};
 
 function display(text) {
 
-}
+};
