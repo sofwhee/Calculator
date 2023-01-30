6dc4cbe427ef530764d268eb8f6fb3195bf404ac
sofwhee <sofiamsola@outlook.com>
2023-01-30 13:35:54 -0500

    Replaced childNodes with children in sidebutsarray

--- calculator.js
+++ calculator.js
@@ -40,13 +40,13 @@ midButsArray.forEach(button => button.className = 'numberButton')
 document.getElementById('decimalButton').classList.remove('numberButton')
 document.getElementById('equalsButton').classList.remove('numberButton')
 
-const sideButCont = document.querySelector('#sideButtonsContainer')
-const sideButsArray = Array.from(sideButCont.childNodes)
-sideButsArray.forEach(button => button.className = 'sideButton')
+const sideButCont = document.querySelector('#sideButtonsContainer');
+const sideButsArray = Array.from(sideButCont.children);
+sideButsArray.forEach(button => button.classList.add("sideButton"));
 
 // make number buttons put numbers on calculator
 function numberPress(buttPressed) {
-	textToDisplay = document.getElementById('display').innerText
+	let textToDisplay = document.getElementById('display').innerText
 	textToDisplay += buttPressed.innerText
 	display(textToDisplay)
 }
@@ -66,7 +66,25 @@ const clearBut = document.getElementById('clearButton')
 clearBut.addEventListener('click', clear)
 
 // make operand buttons put operands on calculator
+function operandPress(buttPressed) {
+	let operandText = buttPressed.innerText
+	let displayText = document.getElementById('display').innerText
 
+	console.log(operandText)
+
+	if (displayText.includes(operandText)) {
+		const numbers = displayText.split(operandText)
+		operate(numbers[0], numbers[1], operandText)
+	} else {
+		numberPress(buttPressed)
+	}
+}
+
+const operandButs = document.querySelectorAll('.operand')
+const operandButsArray = Array.from(operandButs)
+operandButsArray.forEach((operandBut) => {
+	operandBut.addEventListener('click', operandPress.bind(null, operandBut))
+})
 // make equals button execute operation in display
 
 // make operand buttons automatically put through
